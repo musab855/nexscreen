f6767245cefef9f5c94660dcf05b43dc6a788aad
musab855 <musabasif5@gmail.com>
2026-06-15 12:50:42 +0530

    fix: add Vercel domain to CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://nexscreen-nu.vercel.app",
+                ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
